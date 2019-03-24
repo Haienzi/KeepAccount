dd65632de0189dad17afb339b21dd387fc966f1e
Maqiuhong <maqiuhong@vip.henu.edu.cn>
2019-03-24 21:27:06 +0800

    账本界面布局

---
+++ app/src/main/java/com/qiu/keepaccount/activity/BookActivity.java
@@ -0,0 +1,20 @@
+package com.qiu.keepaccount.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qiu.keepaccount.R;
+
+/**
+ * @Author qiuhong.ma
+ * @Date 2019/3/24 16:07
+ * @Description 账本页面
+ */
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+    }
+}
