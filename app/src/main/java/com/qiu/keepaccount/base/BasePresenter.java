6032d34a3332492527c3312ac660118a8d314153
Haienzi <2835595142@qq.com>
2019-04-03 10:59:26 +0800

    添加activity管理类和工具类

---
+++ app/src/main/java/com/qiu/keepaccount/base/BasePresenter.java
@@ -0,0 +1,14 @@
+package com.qiu.keepaccount.base;
+
+/**
+ * @Author qiuhong.ma
+ * @Date 2019/4/3 10:32
+ * @Description presenter统一接口
+ */
+public interface BasePresenter {
+
+    /**
+     * 数据加载初始化等
+     */
+    void start();
+}
